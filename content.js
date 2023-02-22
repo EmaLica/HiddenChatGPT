ada0fc1ef856cd43e5b9f18f9d21f1e70014d7be
Emanuele Licata <94196543+EmaLica@users.noreply.github.com>
2023-02-22 19:25:24 +0100

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.commands.onCommand.addListener(function(command) {
-    if (command === 'highlighted-text') {
-      chrome.tabs.executeScript({
-        code: 'var selectedText = window.getSelection().toString(); if (selectedText !== "") { alert("You highlighted the following text: " + selectedText); }'
-      });
-    }
-  });
-  
